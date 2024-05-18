06f11ef6c4e7940b6a400b89f6cfa2d198f0c0c3
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-18 16:14:15 -0400

    chore: Update cors.js to include new accepted origin for localhost:5173

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,7 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
   'http://localhost:3000',
   'https://myfrontend.com',
-	'https://localhost:5173'
+	'http://localhost:5173',
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
