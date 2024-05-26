21ef57f6368bc0c36103b5d55062dc9f570cc68c
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-26 16:26:51 -0400

    feat: Remove unused getById method from MovieController

--- controllers/movies.js
+++ controllers/movies.js
@@ -8,13 +8,6 @@ export class MovieController {
 		const movies = await MovieModel.getAll({ genre })
 		res.json(movies)
 	}
-	static async getById(req, res) {
-		const { id } = req.params
-		const movie = await MovieModel.getById({ id })
-		if (movie) return res.json(movie)
-
-		res.status(404).json({ message: 'Movie not found' })
-	}
 
 	static async create(req, res) {
 		const result = validateMovie(req.body)
